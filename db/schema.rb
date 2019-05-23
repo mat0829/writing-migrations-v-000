95452bcd230b96101630934165997e936fd7aa9e
David Anderson <matandersonlv@hotmail.com>
2019-05-23 23:01:03 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
-
-  create_table "artists", force: :cascade do |t|
-    t.text "name"
-    t.text "genre"
-    t.integer "age"
-    t.text "hometown"
-  end
+ActiveRecord::Schema.define(version: 3) do
 
   create_table "students", force: :cascade do |t|
-    t.text "name"
+    t.string "name"
     t.integer "grade"
-    t.string "birthdate"
+    t.datetime "birthdate"
   end
 
 end
